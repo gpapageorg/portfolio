f47907b4024f65fb0f95f9e884779ec852888579
george <georg.papageorg@gmail.com>
2023-10-29 18:26:27 +0200

    removed unnecessary devider

--- src/Components/Projects/index.js
+++ src/Components/Projects/index.js
@@ -1,32 +1,49 @@
-import React from 'react'
-import { useState } from 'react'
-import { Container, Wrapper, Title, Desc, CardContainer, ToggleButtonGroup, ToggleButton } from './ProjectsStyle'
-import ProjectCard from '../Cards/ProjectCards'
-import { projects } from '../../Data/constants'
-import { Divider } from './ProjectsStyle'
+import React from "react";
+import { useState } from "react";
+import {
+  Container,
+  Wrapper,
+  Title,
+  Desc,
+  CardContainer,
+  ToggleButtonGroup,
+  ToggleButton,
+} from "./ProjectsStyle";
+import ProjectCard from "../Cards/ProjectCards";
+import { projects } from "../../Data/constants";
+import { Divider } from "./ProjectsStyle";
 
-const Projects = ({openModal,setOpenModal}) => {
-  const [toggle, setToggle] = useState('all');
+const Projects = ({ openModal, setOpenModal }) => {
+  const [toggle, setToggle] = useState("all");
   return (
     <Container id="projects">
       <Wrapper>
         <Title>Projects</Title>
-        <Desc>
-          Desc To Be Filled
-        </Desc>
-        <ToggleButtonGroup >
-          {toggle === 'all' ?
-            <ToggleButton active value="all" onClick={() => setToggle('all')}>All</ToggleButton>
-            :
-            <ToggleButton value="all" onClick={() => setToggle('all')}>All</ToggleButton>
-          }
+        <Desc>Desc To Be Filled</Desc>
+        <ToggleButtonGroup>
+          {toggle === "all" ? (
+            <ToggleButton active value="all" onClick={() => setToggle("all")}>
+              All
+            </ToggleButton>
+          ) : (
+            <ToggleButton value="all" onClick={() => setToggle("all")}>
+              All
+            </ToggleButton>
+          )}
           <Divider />
-          {toggle === 'web app' ?
-            <ToggleButton active value="web app" onClick={() => setToggle('web app')}>Web Apps</ToggleButton>
-            :
-            <ToggleButton value="web app" onClick={() => setToggle('web app')}>Web Apps</ToggleButton>
-          }
-          <Divider />
+          {toggle === "web app" ? (
+            <ToggleButton
+              active
+              value="web app"
+              onClick={() => setToggle("web app")}
+            >
+              Web Apps
+            </ToggleButton>
+          ) : (
+            <ToggleButton value="web app" onClick={() => setToggle("web app")}>
+              Web Apps
+            </ToggleButton>
+          )}
           {/* {toggle === 'general' ?
             <ToggleButton active value="android app" onClick={() => setToggle('android app')}>General</ToggleButton>
             :
@@ -34,20 +51,27 @@ const Projects = ({openModal,setOpenModal}) => {
           } */}
         </ToggleButtonGroup>
         <CardContainer>
-          {toggle === 'all' && projects
-            .map((project) => (
-              <ProjectCard project={project} openModal={openModal} setOpenModal={setOpenModal}/>
+          {toggle === "all" &&
+            projects.map((project) => (
+              <ProjectCard
+                project={project}
+                openModal={openModal}
+                setOpenModal={setOpenModal}
+              />
             ))}
           {projects
             .filter((item) => item.category === toggle)
             .map((project) => (
-              <ProjectCard project={project} openModal={openModal} setOpenModal={setOpenModal}/>
+              <ProjectCard
+                project={project}
+                openModal={openModal}
+                setOpenModal={setOpenModal}
+              />
             ))}
-            
         </CardContainer>
       </Wrapper>
     </Container>
-  )
-}
+  );
+};
 
 export default Projects;
